a0a3a76960a66fd066ae9265ef5c6e6221d1c092
RhonaJoy <rhonajoy8@gmail.com>
2023-10-16 19:44:18 +0300

    add file

---
+++ apple/macbookpro.py
@@ -0,0 +1,15 @@
+from ..hp import model
+
+print(model)
+# class MacbookLaptop:
+#     def __init__(self, model, processor, memory, storage):
+#         self.model = model
+#         self.processor = processor
+#         self.memory = memory
+#         self.storage = storage
+
+#     def display_info(self):
+#         print(f"Model: {self.model}")
+#         print(f"Processor: {self.processor}")
+#         print(f"Memory: {self.memory} GB")
+#         print(f"Storage: {self.storage} GB")
---
+++ hp.py
@@ -0,0 +1,19 @@
+
+print("This is a Hp")
+def model():
+    print("I am the latest model")
+
+
+
+# class HPLaptop:
+#     def __init__(self, model, processor, memory, storage):
+#         self.model = model
+#         self.processor = processor
+#         self.memory = memory
+#         self.storage = storage
+
+#     def display_info(self):
+#         print(f"Model: {self.model}")
+#         print(f"Processor: {self.processor}")
+#         print(f"Memory: {self.memory} GB")
+#         print(f"Storage: {self.storage} GB")
